430f6235b414be5ef7715b9b56ebc3908107427f
cvaske2 <colevaske@outlook.com>
2023-05-01 10:29:26 -0500

    Basic input parameter handling

--- adr-gen.py
+++ adr-gen.py
@@ -1,9 +1,20 @@
+import sys
 from tkinter import *
 from tkinter import ttk
+
+# Grab args and slice off first (name of executable)
+ARGS = sys.argv[1:]
+
+
+if len(ARGS) != 0 and ARGS[0] in ['help', '-help', '--help', '-h', '--h']:
+    print("Here are some parameters available to you:") # TODO: implement
+    print()
+    exit()
 
 root = Tk()
 frame = ttk.Frame(root, padding=10)
 frame.grid()
+
 
 ttk.Label(frame, text="Hello").grid(column=0, row=0)
 ttk.Button(frame, text="Exit", command=root.destroy).grid(column=0, row=1)
