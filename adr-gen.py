3e15d39bd07859dcdb4a88118533717baea86f33
Manoj Reddy Oladri <41293750+manojreddyoladri@users.noreply.github.com>
2023-05-01 06:15:59 +0000

    Update adr-gen.py

--- adr-gen.py
+++ adr-gen.py
@@ -1,11 +1,23 @@
 from tkinter import *
 from tkinter import ttk
+from  peer_to_peer_adr import *
+from layered_adr import *
+
+def open_peer_to_peer_style():
+    root = Tk()
+    peer_to_peer_frame(root)
+
+def open_layered_style():
+    root = Tk()
+    layered_frame(root)
 
 root = Tk()
 frame = ttk.Frame(root, padding=10)
 frame.grid()
 
 ttk.Label(frame, text="Hello").grid(column=0, row=0)
-ttk.Button(frame, text="Exit", command=root.destroy).grid(column=0, row=1)
+ttk.Button(frame, text="Peer to Peer Style frame", command=open_peer_to_peer_style).grid(column=0, row=1)
+ttk.Button(frame, text="Layered Style frame", command=open_layered_style).grid(column=0, row=2)
+
 
 root.mainloop()
