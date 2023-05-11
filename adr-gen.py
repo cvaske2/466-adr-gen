47ac8bf5c4abf4dd48ac6eb8520ce700aab5803f
cvaske2 <colevaske@outlook.com>
2023-05-11 00:09:07 -0500

    WIP: Parameters. Currently accepting '--help', implementing style specifications

--- adr-gen.py
+++ adr-gen.py
@@ -5,7 +5,26 @@ from datetime import date
 import adr_windows
 
 # globals - also act as environment variables
-ADR_DIR = "/adr-test/"
+ADR_DIR = "./adr-test/"
+
+VALID_FLAGS = {
+    "help": ['help', '-help', '--help', '-h', '--h'],
+    "style": ['--style', '-style', '--s', '-s'],
+    "omit": ['--omit', '-omit', '--remove', '-remove', '--rem', '-rem', '--no', '-no']
+}
+
+VALID_PARAMS = {
+    "Client Server": ["client server", "client-server", "clientserver", "client&server", "cs", "c-s", "c&s"],
+    "Data Flow": ["data flow", "data-flow", "dataflow", "d-f", "df"],
+    "Event-based": ["event based", "event-based", "eventbased", "e-b", "eb", "event"],
+    "Layered": ["layered"],
+    "Main Program & Subroutine": ["main program & subroutine", "mainprogram&subroutine", "mainprogram", "subroutine", "mp&s", "p&s"],
+    "Object Oriented": ["object oriented", "object-oriented", "object", "o-o", "oo", "oop"],
+    "Peer-to-peer": ["peer to peer", "peer-to-peer", "peertopeer", "peer2peer", "p-t-p", "ptp", "p2p", "pp"],
+    "Pipe & Filter": ["pipe & filter", "pipe and filter", "pipe-&-filter", "pipe-and-filter", "pipe&filter", "pipeandfilter", "p-&-f", "p-a-f", "p&f", "paf", "pf", "pipe", "filter"],
+    "Publish-Subscribe": ["publish subscribe", "publish-subscribe", "publishsubscribe", "publish/subscribe", "p-s", "ps", "p/s", "publish", "subscribe", "pub", "sub", "pubsub"],
+    "Rule-based": ["rule based", "rule-based", "rulebased", "r-b", "rb", "rule", "rules"]
+}
 
 def split_string(template_string):
     '''
@@ -43,7 +62,7 @@ def open_style_template(template_path):
         data = file.read()
     return split_string(data)
 
-def open_window(template_data, style):
+def open_subwindow(template_data, style, selection = None):
     '''
     Opens a window and adds content based on the data read from the template.
     Parameter: "template_data" - a diciontary of the form { heading: "<template content suggestion>", ... }
@@ -69,10 +88,10 @@ def open_window(template_data, style):
         heading_flags[heading].state(["!alternate", "selected"])
         row_index += 1
     
-    ttk.Button(subroot, text="Done", command=lambda: generate_template(heading_flags, template_data, style)).grid(column=0, row=row_index)
+    ttk.Button(subroot, text="Done", command=lambda: generate_template_from_gui(heading_flags, template_data, style)).grid(column=0, row=row_index)
     ttk.Button(subroot, text="Cancel", command=subroot.destroy).grid(column=1, row=row_index)
 
-def generate_template(headings, template, style):
+def generate_template_from_gui(headings, template, style):
     current_date = date.today().strftime('%d%b%Y')
     output_string = f"# {style} decision on {current_date}\n\n"
 
@@ -87,75 +106,85 @@ def generate_template(headings, template, style):
 
 def open_client_server_style():
     template_content = open_style_template("./adr-templates/client-server-style.md")
-    open_window(template_content, "Client Server")
+    open_subwindow(template_content, "Client Server")
 
 def open_data_flow_style():
     template_content = open_style_template("./adr-templates/data-flow-style.md")
-    open_window(template_content, "Data Flow")
+    open_subwindow(template_content, "Data Flow")
 
 def open_event_based_style():
     template_content = open_style_template("./adr-templates/event-based-style.md")
-    open_window(template_content, "Event-based")
+    open_subwindow(template_content, "Event-based")
 
 def open_layered_style():
     template_content = open_style_template("./adr-templates/layered-style.md")
-    open_window(template_content, "Layered")
+    open_subwindow(template_content, "Layered")
 
 def open_main_program_and_subroutine_style():
     template_content = open_style_template("./adr-templates/main-program-and-subroutine-style.md")
-    open_window(template_content, "Main Program & Subroutine")
+    open_subwindow(template_content, "Main Program & Subroutine")
 
 def open_object_oriented_style():
     template_content = open_style_template("./adr-templates/object-oriented-style.md")
-    open_window(template_content, "Object Oriented")
+    open_subwindow(template_content, "Object Oriented")
 
 def open_peer_to_peer_style():
     template_content = open_style_template("./adr-templates/peer-to-peer-style.md")
-    open_window(template_content, "Peer-to-peer")
+    open_subwindow(template_content, "Peer-to-peer")
 
 def open_pipe_and_filter_style():
     template_content = open_style_template("./adr-templates/pipe-and-filter-style.md")
-    open_window(template_content, "Pipe & Filter")
+    open_subwindow(template_content, "Pipe & Filter")
 
 def open_publish_subscribe_style():
     template_content = open_style_template("./adr-templates/publish-subscribe-style.md")
-    open_window(template_content, "Publish-Subscribe")
+    open_subwindow(template_content, "Publish-Subscribe")
 
 def open_rule_based_style():
     template_content = open_style_template("./adr-templates/rule-based-style.md")
-    open_window(template_content, "Rule-based")
+    open_subwindow(template_content, "Rule-based")
 
 # def create_new_window():
 #     adr_windows.open_cs()
 
+def open_window():
+    root = Tk()
+    frame = ttk.Frame(root, padding=10)
+    frame.grid()
+
+    root.title("Template Select")
+
+    ttk.Label(frame, text="Select from a style template").grid(column=0, row=0)
+    ttk.Button(frame, text="Client Server Style", command=open_client_server_style).grid(column=0, row=1)
+    ttk.Button(frame, text="Data Flow", command=open_data_flow_style).grid(column=0, row=2)
+    ttk.Button(frame, text="Event Based", command=open_event_based_style).grid(column=0, row=3)
+    ttk.Button(frame, text="Layered Style", command=open_layered_style).grid(column=0, row=4)
+    ttk.Button(frame, text="Main Program & Subroutine Style", command=open_main_program_and_subroutine_style).grid(column=0, row=5)
+    ttk.Button(frame, text="Object Oriented Style", command=open_object_oriented_style).grid(column=0, row=6)
+    ttk.Button(frame, text="Peer to Peer Style", command=open_peer_to_peer_style).grid(column=0, row=7)
+    ttk.Button(frame, text="Pipe and Filter Style", command=open_pipe_and_filter_style).grid(column=0, row=8)
+    ttk.Button(frame, text="Publish/Subscribe Style", command=open_publish_subscribe_style).grid(column=0, row=9)
+    ttk.Button(frame, text="Rule-Based Style", command=open_rule_based_style).grid(column=0, row=10)
+    ttk.Button(frame, text="Client Server Style", command=lambda: adr_windows.open_cs()).grid(column=0, row=11)
+    ttk.Button(frame, text="tk event_based", command=lambda: adr_windows.event_based_template()).grid(column=0, row=12)
+
+    root.mainloop()
 
 # Grab args and slice off first (name of executable)
 ARGS = sys.argv[1:]
+print(ARGS)
 
-
-if len(ARGS) != 0 and ARGS[0] in ['help', '-help', '--help', '-h', '--h']:
-    print("Here are some parameters available to you:") # TODO: implement
-    print()
+if len(ARGS) == 0:
+    open_window()
+elif ARGS[0] in VALID_FLAGS['help']:
+    print("#"*50)
+    print("HELP:")
+    print("Here are some parameters available to you:")
+    print(f"{VALID_FLAGS['style']} \"{list(VALID_PARAMS.keys())}\"")
+    print(f"{VALID_FLAGS['omit']} \"<heading(s)>\"")
+    print("\nFor example, 'adr-gen --s peer2peer --rem references' will generate a peer-to-peer style template with no 'references' section.")
+    print("#"*50)
     exit()
-
-root = Tk()
-frame = ttk.Frame(root, padding=10)
-frame.grid()
-
-root.title("Template Select")
-
-ttk.Label(frame, text="Select from a style template").grid(column=0, row=0)
-ttk.Button(frame, text="Client Server Style", command=open_client_server_style).grid(column=0, row=1)
-ttk.Button(frame, text="Data Flow", command=open_data_flow_style).grid(column=0, row=2)
-ttk.Button(frame, text="Event Based", command=open_event_based_style).grid(column=0, row=3)
-ttk.Button(frame, text="Layered Style", command=open_layered_style).grid(column=0, row=4)
-ttk.Button(frame, text="Main Program & Subroutine Style", command=open_main_program_and_subroutine_style).grid(column=0, row=5)
-ttk.Button(frame, text="Object Oriented Style", command=open_object_oriented_style).grid(column=0, row=6)
-ttk.Button(frame, text="Peer to Peer Style", command=open_peer_to_peer_style).grid(column=0, row=7)
-ttk.Button(frame, text="Pipe and Filter Style", command=open_pipe_and_filter_style).grid(column=0, row=8)
-ttk.Button(frame, text="Publish/Subscribe Style", command=open_publish_subscribe_style).grid(column=0, row=9)
-ttk.Button(frame, text="Rule-Based Style", command=open_rule_based_style).grid(column=0, row=10)
-ttk.Button(frame, text="Client Server Style", command=lambda: adr_windows.open_cs()).grid(column=0, row=11)
-ttk.Button(frame, text="tk event_based", command=lambda: adr_windows.event_based_template()).grid(column=0, row=12)
-
-root.mainloop()
+elif ARGS[0] in VALID_FLAGS['style']:
+    print(ARGS)
+    print(VALID_PARAMS)
